5ae2c2958038692c695f4daf8e327db219a8fe77
Kaung Myat Lwin <kaungmyatlwin@burmese.ai>
2020-07-23 23:42:02 +0630

    fix: Result Page 404

---
+++ pages/results/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout/Layout";
+
+const Results = (props) => {
+  return (
+    <Layout>
+      This is Results Page.
+    </Layout>
+  );
+}
+
+export default Results;
