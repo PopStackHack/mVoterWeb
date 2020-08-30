ef1a06baf7a5c8a3e68d37c98cf52c5136cee5fa
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-08-31 00:25:19 +0630

    refactor: Parties Page with proper data binding

--- pages/parties/index.js
+++ pages/parties/index.js
@@ -1,53 +1,89 @@
-import { PureComponent, useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Head from 'next/head';
-import Layout from '../../components/Layout/Layout';
-import PartyCard from '../../components/Parties/PartyItem';
-import FlagFillIcon from '../../components/Common/Icons/flagFill';
-import { getParty } from '../../gateway/api';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import './parties.module.scss';
+import Layout from '../../components/Layout/Layout';
+import PartyItem from '../../components/Parties/PartyItem';
+import FlagFillIcon from '../../components/Common/Icons/flagFill';
+import { getParties } from '../../gateway/api';
 import AppHeader from '../../components/Layout/AppHeader/AppHeader';
 import Button from '../../components/Common/Button/Button';
 import Link from '../../components/Common/Link/Link';
-import InfiniteScroll from 'react-infinite-scroll-component';
+
+import './parties.module.scss';
 
 const Parties = (props) => {
-  const rowRef = useRef();
   // Inject AJAX call on first load
+  const [parties, setParties] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    // initial load
+    fetchAndPushParties(true);
+  }, []);
+
+  async function fetchAndPushParties(init = false) {
+    try {
+      let pageQuery = page;
+      if (!init) {
+        pageQuery += 1;
+        setPage(page + 1);
+      }
+
+      const response = await fetch(`/api/parties?page=${pageQuery}`);
+      const { data, pagination } = await response.json();
+
+      setParties(parties.concat(data));
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response);
+      }
+    }
+  }
 
   return (
     <Layout>
       <Head>
-        <title>ပါတီများ</title>
+        <title>ပါတီများ | mVoter 2020</title>
       </Head>
       <AppHeader>
-        <div>ပါတီများ</div>
+        <div className="text-bold">ပါတီများ</div>
         <div className="Parties__buttonGroup">
           <i className="material-icons">search</i>
         </div>
       </AppHeader>
-      <div className="Parties container">
-        <div className="row mb-2">
-          <div className="col-12">
-            <div className="Parties__infoHeader">
-              {/* <FlagFillIcon /> */}
-              <p>
-                (၂၀၂၀) ခုနှစ် အထွေထွေ ရွေးကောက်ပွဲတွင် ဝင်ရောက်ယှဥ်ပြိုင်မည့်
-                နိုင်ငံရေးပါတီ (၉၅)ခု
-              </p>
+        <div id="Parties" className="Parties container">
+          <div className="row mb-2">
+            <div className="col-12">
+              <div className="Parties__infoHeader">
+                {/* <FlagFillIcon /> */}
+                <p>
+                  (၂၀၂၀) ခုနှစ် အထွေထွေ ရွေးကောက်ပွဲတွင် ဝင်ရောက်ယှဥ်ပြိုင်မည့်
+                  နိုင်ငံရေးပါတီ (၉၅)ခု
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <InfiniteScroll
+                scrollableTarget="Parties"
+                next={fetchAndPushParties}
+                dataLength={parties.length}
+                hasMore={true}>
+                <ul className="Parties__list">
+                {
+                  parties.map((party) => {
+                    return (
+                      <PartyItem key={party.id} party={party} />
+                    );
+                  })
+                }
+                </ul>
+              </InfiniteScroll>
             </div>
           </div>
         </div>
-        <div className="row">
-          {/* <InfiniteScroll> */}
-            {[0, 1, 2].map((c, i) => (
-              <div className="col-12" key={i}>
-                <PartyCard />
-              </div>
-            ))}
-          {/* </InfiniteScroll> */}
-        </div>
-      </div>
     </Layout>
   );
 }
