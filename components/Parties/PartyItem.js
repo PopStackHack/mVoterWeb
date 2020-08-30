86aaf03906d7466322a4a00cf73510e70ac24251
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-08-31 00:20:19 +0630

    feat: party item update

--- components/Parties/PartyItem.js
+++ components/Parties/PartyItem.js
@@ -7,24 +7,29 @@ import { route } from 'next/dist/next-server/server/router';
 const PartyCard = (props) => {
   const router = useRouter();
   const {
-    id = 1,
-    name,
-    eng_name,
-    party_seal
+    party: {
+      id,
+      attributes: {
+        name_burmese: nameBurmese,
+        region,
+      }
+    },
   } = props;
 
   return (
-    <Card className="PartyItem cursor-pointer" onClick={() => router.push(`/parties/[party]`, `/parties/${id}`)}>
-      <div style={{
-        width: 64,
-        height: 64,
-        backgroundImage: `url("//via.placeholder.com/150}")`
-      }}></div>
-      <div className="PartyItem__Description">
-        <div>ဖရဲသီးဖွံ့ဖြိုးတိုးတက်ရေးပါတီ</div>
-        <div className="text-muted constituency">ပြည်ထောင်စုတစ်ဝန်းလုံး</div>
-      </div>
-    </Card>
+    <li className="PartyItem">
+      <Card className="PartyItem__Card cursor-pointer" onClick={() => router.push(`/parties/[party]`, `/parties/${id}`)}>
+        <div style={{
+          width: 64,
+          height: 64,
+          backgroundImage: `url("//via.placeholder.com/150}")`
+        }}></div>
+        <div className="PartyItem__Description">
+          <div className="name">{nameBurmese}</div>
+          <div className="text-muted constituency">{region}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
