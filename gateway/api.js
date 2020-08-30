73f0c706468752bc7d135293295be0b310ed2865
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-08-31 00:20:54 +0630

    refactor: rewrite gateway to class structure

--- gateway/api.js
+++ gateway/api.js
@@ -1,112 +1,119 @@
 import axios from 'axios';
 
-const maePaySohAPI = axios.create({
-  baseURL: process.env.BASE_URL,
-  timeout: 10000,
-  headers: {
-    'api-key': process.env.API_KEY,
-  },
-});
+class MaePaySohAPI {
+  constructor(token) {
+    this.token = token;
+    this.api = axios.create({
+      baseURL: process.env.BASE_URL,
+      timeout: 10000,
+      headers: {
+        'api-token': token,
+      },
+    });
+  }
 
-export function getCandidates({
-  wardId,
-  house,
-  name,
-  itemsPerPage = 25,
-  page,
-}) {
-  return maePaySohAPI.get('/candidates', {
-    params: {
-      ward_id: wardId,
-      house,
-      name,
-      items_per_page: itemsPerPage,
-      page,
-    },
-  })
-    .catch(console.error);
-}
+  static getCandidates({
+    wardId,
+    house,
+    name,
+    itemsPerPage = 25,
+    page,
+  }) {
+    return this.api.get('/candidates', {
+      params: {
+        ward_id: wardId,
+        house,
+        name,
+        items_per_page: itemsPerPage,
+        page,
+      },
+    })
+      .catch(console.error);
+  }
 
-export function getCandidateById(id) {
-  return maePaySohAPI.get(`/candidates/${id}`)
-    .catch(console.error);
-}
+  static getCandidateById(id) {
+    return this.api.get(`/candidates/${id}`)
+      .catch(console.error);
+  }
 
-export function getBallots() {
-  return maePaySohAPI.get('/ballots')
-    .catch(console.error);
-}
+  static getBallots() {
+    return this.api.get('/ballots')
+      .catch(console.error);
+  }
 
-export function getFAQ({
-  page,
-  itemPerPage = 25,
-  name,
-  category,
-}) {
-  return maePaySohAPI.get('/faq', {
-    params: {
-      page,
-      item_per_page: itemPerPage,
-      name,
-      category,
-    }
-  })
-    .catch(console.error);
-}
-
-export function getFAQById(id) {
-  return maePaySohAPI.get(`/faqs/${id}`)
-    .catch(console.error);
-}
-
-export function getStateRegion() {
-  return maePaySohAPI.get('/locality/state_regions')
-    .catch(console.error);
-}
-
-export function getTownships(stateRegionPCode) {
-  return maePaySohAPI.get('/townships', {
-    params: {
-      state_region_pcode: stateRegionPCode,
-    }
-  })
-    .catch(console.error);
-}
-
-export function getWards(townshipPcode) {
-  return maePaySohAPI.get('/wards', {
-    params: {
-      township_pcode: townshipPcode,
-    }
-  })
-    .catch(console.error);
-}
-
-export function getNews({
+  static getFAQ({
     page,
     itemPerPage = 25,
-}) {
-  return maePaySohAPI.get('/news', {
-    params: {
+    name,
+    category,
+  }) {
+    return this.api.get('/faq', {
+      params: {
+        page,
+        item_per_page: itemPerPage,
+        name,
+        category,
+      }
+    })
+      .catch(console.error);
+  }
+
+  static getFAQById(id) {
+    return this.api.get(`/faqs/${id}`)
+      .catch(console.error);
+  }
+
+  static getStateRegion() {
+    return this.api.get('/locality/state_regions')
+      .catch(console.error);
+  }
+
+  static getTownships(stateRegionPCode) {
+    return this.api.get('/townships', {
+      params: {
+        state_region_pcode: stateRegionPCode,
+      }
+    })
+      .catch(console.error);
+  }
+
+  static getWards(townshipPcode) {
+    return this.api.get('/wards', {
+      params: {
+        township_pcode: townshipPcode,
+      }
+    })
+      .catch(console.error);
+  }
+
+  static getNews({
       page,
-      item_per_page: itemPerPage,
-    }
-  })
-    .catch(console.error);
+      itemPerPage = 25,
+  }) {
+    return this.api.get('/news', {
+      params: {
+        page,
+        item_per_page: itemPerPage,
+      }
+    })
+      .catch(console.error);
+  }
+
+  getParties({
+    page,
+  }) {
+    return this.api.get('/parties', {
+      params: {
+        page,
+      },
+    })
+      .catch(console.error);
+  }
+
+  static getPartyById(id) {
+    return this.api.get(`parties/${id}`)
+      .catch(console.error);
+  }
 }
 
-export function getParty({
-  page,
-}) {
-  return maePaySohAPI.get('/parties', {
-    params: {
-      page,
-    },
-  })
-    .catch(console.error);
-}
-
-export function getPartyById(id) {
-  return maePaySohAPI.get(`parties/${id}`)
-    .catch(console.error);
-}
+module.exports = MaePaySohAPI;
