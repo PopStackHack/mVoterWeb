b8fc6e86f1f9fea8901a240d5b33bf9bb3163573
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-08-31 15:53:18 +0630

    refactor: textFormatters to complient with object constants

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+export const FAQ_CATEGORY = Object.freeze({
+  voter_list: 'မဲဆန္ဒရှင်စာရင်း',
+  candidate: 'ကိုယ်စားလှယ်လောင်း',
+  international_observer: 'စောင့်ကြည့်လေ့လာခြင်း',
+  diplomatic: 'သံတမန်ရေးရာ',
+});
+
+export const HOUSES = Object.freeze({
+  amyota: 'အမျိုးသားလွှတ်တော်',
+  pyithu: 'ပြည်ထောင်စုလွှတ်တော်',
+  state: 'တိုင်း/ပြည်နယ်လွှတ်တော်',
+});
--- utils/textFormatter.js
+++ utils/textFormatter.js
@@ -1,15 +1,7 @@
+import { HOUSES, FAQ_CATEGORY } from './constants';
+
 export function formatHouse(house) {
-  let str = 'Unknown';
-
-  if (house === 'amyotha') {
-    str = 'အမျိုးသားလွှတ်တော်';
-  } else if (house === 'pyithu') {
-    str = 'ပြည်ထောင်စုလွှတ်တော်'
-  } else if (house === 'state') {
-    str = 'တိုင်း/ပြည်နယ်လွှတ်တော်';
-  }
-
-  return str;
+  return HOUSES[house];
 }
 
 export function formatConstituency(stateRegion, constituencyName) {
@@ -17,18 +9,5 @@ export function formatConstituency(stateRegion, constituencyName) {
 }
 
 export function formatFAQCategory(category) {
-  let str = 'Unknown';
-
-  // Thanks but I hate switch cases for no reason
-  if (category === 'voter_list') {
-    return 'မဲဆန္ဒရှင်စာရင်း';
-  } else if (category === 'candidate') {
-    return 'ကိုယ်စားလှယ်လောင်း';
-  } else if (category === 'international_observer') {
-    return 'စောင့်ကြည့်လေ့လာခြင်း';
-  } else {
-    return 'သံတမန်ရေးရာ'; // diplomatic
-  }
-
-  return str;
+  return FAQ_CATEGORY[category];
 }
