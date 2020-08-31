0535ac93a020ec73807cb55e819a2e825e14f3e8
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-01 00:11:36 +0630

    refactor: text formatter publishedDate

--- utils/textFormatter.js
+++ utils/textFormatter.js
@@ -1,4 +1,6 @@
 import { HOUSES, FAQ_CATEGORY } from './constants';
+import myanmarNumbers from 'myanmar-numbers';
+import moment from 'moment';
 
 export function formatHouse(house) {
   return HOUSES[house];
@@ -10,4 +12,16 @@ export function formatConstituency(stateRegion, constituencyName) {
 
 export function formatFAQCategory(category) {
   return FAQ_CATEGORY[category];
+}
+
+// Nasty format function to quickly achieve our MM locale result
+// dateStr will be in YYYY-MM-DD format
+export function formatPublishDateToMMLocale(dateStr) {
+  const publishedTime = moment(dateStr);
+  const date = dateStr.split('-')[2]; // It has zero paddings so we cheat.
+  const month = publishedTime.month(); // This is to access MM Locale months below
+  const year = publishedTime.year();
+  // Text data taken from https://gist.github.com/eimg/3f115b39fbc6c48976e6cc7af1b8ddf9
+  const months = 'ဇန်နဝါရီ_ဖေဖော်ဝါရီ_မတ်_ဧပြီ_မေ_ဇွန်_ဇူလိုင်_သြဂုတ်_စက်တင်ဘာ_အောက်တိုဘာ_နိုဝင်ဘာ_ဒီဇင်ဘာ'.split('_');
+  return `${myanmarNumbers(date, 'my')} ${months[month - 1]} ${myanmarNumbers(year, 'my')}`
 }
