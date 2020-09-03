dfa7bbf56abbadbefb9e892bb154fe4f32ae1e2c
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-04 01:36:11 +0630

    fix: Mobile Chrome header consideration

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -24,8 +24,8 @@ const Layout = ({ children, shouldHideBottomNav = false }) => {
         position: relative;
         font-size: 15px;
         width: 100%;
-        height: 100vh;
-        max-height: 100vh;
+        height: 100%;
+        min-height: 100%;
         overflow: hidden;
       }
     `}</style>
