319591acc16a4bad3d0bf202c19a77ca3004b716
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-06 19:27:17 +0630

    chore: Add debounce func

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,13 @@ export function hasFullLocation() {
     localStorage.getItem('township') &&
     localStorage.getItem('wardVillage');
 }
+
+// https://chrisboakes.com/how-a-javascript-debounce-function-works/ <- Code taken from this article
+export function debounce(callback, wait) {
+    let timeout;
+    return (...args) => {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => callback.apply(context, args), wait);
+    };
+}
