818d6b64ee81f222eb763029f6d5256b226f422c
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-15 23:54:49 +0630

    fix: individual candidate preview

--- pages/candidates/[candidate].js
+++ pages/candidates/[candidate].js
@@ -77,13 +77,13 @@ const Candidates = (props) => {
 
         <meta property="og:url" content={`https://web.mvoterapp.com/candidates/${id}`} />
         <meta property="og:type" content="profile" />
-        <meta property="og:title" content={`${name} - ${partyName}`} />
+        <meta property="og:title" content={`${name} - ${partyName ?? 'တစ်သီးပုဂ္ဂလ'}`} />
         <meta property="og:description" content={`${stateRegion}၊ ${constituencyName} တွင် ဝင်ရောက်ယှဥ်ပြိုင်မည်`} />
         <meta property="og:image" content={image} />
 
         <meta property="twitter:card" content="summary" />
         <meta property="twitter:url" content={`https://web.mvoterapp.com/candidates/${id}`} />
-        <meta property="twitter:title" content={`${name} - ${partyName}`} />
+        <meta property="twitter:title" content={`${name} - ${partyName ?? 'တစ်သီးပုဂ္ဂလ'}`} />
         <meta property="twitter:description" content={`${stateRegion}၊ ${constituencyName} တွင် ဝင်ရောက်ယှဥ်ပြိုင်မည်`} />
         <meta property="twitter:image" content={image} />
       </Head>
