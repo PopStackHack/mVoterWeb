ddb0f44d78a5f57bbc569ad792b232bfdb0969b1
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-19 14:36:04 +0630

    fix: Add meta sitename

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ function mVoterApp({ Component, pageProps }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
+        <meta property="og:site_name" content="mVoter 2020" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
