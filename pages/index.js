ab24cf9bfe04171bfe552dd2231f5ed996ae8756
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-28 00:07:40 +0630

    fix: index head

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,23 @@ const Home = () => {
 
   return (
     <div>
+      <Head>
+        <title>mVoter 2020 Web Application</title>
+
+        <meta name="title" content="mVoter 2020 Web Application" />
+        <meta name="description" content="ကိုယ်စားလှယ်လောင်းများ၊ ပါတီများ၊ မဲပေးနည်း၊ သိမှတ်ဖွယ်ရာနှင့် သတင်းများကို အလွယ်တကူ သိရှိနိုင်မယ့် mVoter 2020 App" />
+
+        <meta property="og:url" content="https://web.mvoterapp.com" />
+        <meta property="og:title" content="mVoter 2020 Web Application" />
+        <meta property="og:description" content="(၂၀၂၀) ခုနှစ်၊ အထွေထွေရွေးကောက်ပွဲ အတွက် ကိုယ်စားလှယ်လောင်းများ၊ ပါတီများ၊ မဲပေးနည်း၊ သိမှတ်ဖွယ်ရာနှင့် သတင်းများကို အလွယ်တကူ သိရှိနိုင်မယ့် mVoter 2020 App" />
+        <meta property="og:image" content="/android-chrome-512x512.png" />
+
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content="https://web.mvoterapp.com" />
+        <meta property="twitter:title" content="mVoter 2020 Web Application" />
+        <meta property="twitter:description" content="ကိုယ်စားလှယ်လောင်းများ၊ ပါတီများ၊ မဲပေးနည်း၊ သိမှတ်ဖွယ်ရာနှင့် သတင်းများကို အလွယ်တကူ သိရှိနိုင်မယ့် mVoter 2020 App" />
+        <meta property="twitter:image" content="/android-chrome-512x512.png" />
+      </Head>
       <Layout />
     </div>
   );
