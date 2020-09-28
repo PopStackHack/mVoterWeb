669a745debc2709d59fe0e2e437770e4ea6f4d23
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-09-28 23:59:17 +0630

    feat: News Search placeholders

--- pages/news/search.js
+++ pages/news/search.js
@@ -5,8 +5,10 @@ const NewsSearch = () => {
   return (
     <SearchPage
       type="news"
-      inputPlaceholder="ရှာလိုသော သတင်းခေါင်းစဥ်အား ရိုက်ထည့်ပါ"
       endpoint="searchNews"
+      inputPlaceholder="ရှာလိုသော သတင်းခေါင်းစဥ်အား ရိုက်ထည့်ပါ"
+      emptyPlaceholder="မိမိ ရှာလို့သော သတင်းခေါင်းစဥ်ကို အပေါ်တွင်ရိုက်ထည့်ပါ"
+      notFoundPlaceholder="သင်ရှာသော သတင်းခေါင်းစဥ်အား ရှာမတွေ့ပါ"
     >
       <NewsList />
     </SearchPage>
